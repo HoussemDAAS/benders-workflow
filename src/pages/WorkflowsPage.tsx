57bebafa07e2e91c83460601497c45ab5a6b44fa
Mohamed Slimane <113265001+MedSlimane@users.noreply.github.com>
2025-06-22 15:59:50 +0100

    workflow bug fix

--- src/pages/WorkflowsPage.tsx
+++ src/pages/WorkflowsPage.tsx
@@ -4,7 +4,7 @@ import { useAppContext } from '../hooks/useAppContext';
 import { useWorkflowActions } from '../hooks/useWorkflowActions';
 
 const WorkflowsPage: React.FC = () => {
-  const { workflows, clients, teamMembers } = useAppContext();
+  const { workflows, clients, teamMembers, kanbanTasks } = useAppContext();
   const { createWorkflow, updateWorkflow, deleteWorkflow, updateWorkflowStatus } = useWorkflowActions();
 
   return (
@@ -12,6 +12,7 @@ const WorkflowsPage: React.FC = () => {
       workflows={workflows}
       clients={clients}
       teamMembers={teamMembers}
+      tasks={kanbanTasks}
       onWorkflowCreate={createWorkflow}
       onWorkflowEdit={updateWorkflow}
       onWorkflowDelete={deleteWorkflow}
